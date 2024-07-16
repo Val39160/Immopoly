61c8cee7c19892787f95e5fed923a89fbf0b9c1f
xuan huy le <xuanhuyle@gmail.com>
2024-07-16 10:08:56 +0200

    base

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_15_153526) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_16_072657) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.float "price_per_sqm_livable"
+    t.float "price_per_sqm_land"
+    t.float "price_per_sqm_rental"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
