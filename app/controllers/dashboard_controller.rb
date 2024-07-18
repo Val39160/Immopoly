7f75ce0a58dc24d2da0a27af9e8b65321025e321
asya hambrick <asya.simone@orange.fr>
2024-07-18 17:28:47 +0200

    not yet complete but change for the css button

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def profile
     @projects = Project.where(user: current_user)
-    @properties = Property.where(user: current_user)
+    @properties = Property.where(project: @projects)
   end
 end
