d8c5f7900c90c71a3db2b36566f107249a08cae6
xuan huy le <xuanhuyle@gmail.com>
2024-07-19 16:14:53 +0200

    fix

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -23,8 +23,8 @@ class PropertiesController < ApplicationController
   end
 
   def create
-    @property = Property.new(property_params)
-    if @property.save
+    @property = Property.new(property_params,)
+    if @property.save!
       redirect_to properties_path
     else
       render :new
@@ -34,6 +34,6 @@ class PropertiesController < ApplicationController
   private
 
   def property_params # strong params
-    params.require(:property).permit(:area_sqm_land, :year_construction, :area_sqm_livable, :floor_area_ratio, :nb_dwelling, :density_area, :building_zone)
+    params.require(:property).permit(:city_id, :address, :street_number, :street_name, :zipcode, :building_height_floors, :nb_rooms_per_dwelling, :energy_class, :area_sqm_land, :year_construction, :area_sqm_livable, :floor_area_ratio, :nb_dwelling, :density_area, :building_zone)
   end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,7 +1,7 @@
 class Property < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  belongs_to :project
+  belongs_to :project, optional: true
   belongs_to :city
 
 end
