f46097d94539ceb4cdfa37fd46161bfa8206c1f2
asya hambrick <asya.simone@orange.fr>
2024-07-22 12:24:51 +0200

    slide show still in progress

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -1,16 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
-import Swiper from "swiper"
+import Swiper from "swiper/bundle"
 
 // Connects to data-controller="swiper"
 export default class extends Controller {
   connect() {
-    const swiper = new Swiper(".swiper")
+    const swiper = new Swiper('.swiper')
     new Swiper('.swiper', {
       // Paramètres de Swiper
       slidesPerView: 3,
       spaceBetween: 30,
-      // Ajoutez d'autres options de configuration selon vos besoins
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
     });
+    swiper.init()
     console.log("Hello, Stimulus!", swiper)
   }
 }
